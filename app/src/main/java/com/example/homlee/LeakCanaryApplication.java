d146cb980bfabe9576521d97c2aa57e730fabee6
Leo.Yuan <honglieyuan@163.com>
2019-07-18 06:26:29 +0800

    引入ARouter

--- app/src/main/java/com/example/homlee/LeakCanaryApplication.java
+++ app/src/main/java/com/example/homlee/LeakCanaryApplication.java
@@ -3,6 +3,7 @@ package com.example.homlee;
 import android.app.Application;
 
 import com.example.homlee.utils.MetricsUtils;
+import com.alibaba.android.arouter.launcher.ARouter;
 import com.homlee.mvp.leakcanary.PresenterRefWatcher;
 import com.squareup.leakcanary.LeakCanary;
 import com.squareup.leakcanary.RefWatcher;
@@ -16,12 +17,23 @@ public class LeakCanaryApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        initLeakCanary();
+
+        MetricsUtils.initialize(this);
+        if (BuildConfig.DEBUG) {
+            ARouter.openLog();
+            ARouter.openDebug();
+        }
+        ARouter.init(this);
+    }
+
+    private boolean initLeakCanary() {
         if(LeakCanary.isInAnalyzerProcess(this)) {
-            return;
+            return true;
         }
         RefWatcher watcher = LeakCanary.install(this);
         PresenterRefWatcher.install(watcher);
-        MetricsUtils.initialize(this);
+        return false;
     }
 
 
