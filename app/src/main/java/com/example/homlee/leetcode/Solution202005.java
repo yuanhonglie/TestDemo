d088a28f7512ebfe4d26a881c3848bb55f2980b5
Leo.Yuan <hlyu@royole.com>
2020-05-25 09:15:16 +0800

    leetcode

--- app/src/main/java/com/example/homlee/leetcode/Solution202005.java
+++ app/src/main/java/com/example/homlee/leetcode/Solution202005.java
@@ -1,9 +1,13 @@
 package com.example.homlee.leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution202005 {
+    //===0521===
     /**
      * 求最长回文子串
-     * @date 0522
+     * @date 0521
      * @param s
      * @return
      */
@@ -31,15 +35,109 @@ public class Solution202005 {
         return output;
     }
 
+    //====0522=====
+    private static class TreeNode {
+        int value;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int val) {
+            this.value = val;
+        }
+    }
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+
+        if (preorder == null || inorder == null) {
+            return null;
+        }
+
+        if (preorder.length != inorder.length) {
+            return null;
+        }
+
+        int n = inorder.length;
+
+        Map<Integer, Integer> indexMap = new HashMap<>();
+        for (int i = 0; i < inorder.length; i++) {
+            indexMap.put(inorder[i], i);
+        }
+
+        return buildTreeInner(indexMap, preorder, 0, n - 1, 0, n - 1);
+    }
+
+    private TreeNode buildTreeInner(Map<Integer, Integer> map, int[] preorder, int preorderLeft, int preorderRight, int inorderLeft, int inorderRight) {
+        if (preorderLeft > preorderRight) {
+            return null;
+        }
+
+        int rootValue = preorder[preorderLeft];
+        int inorderIndex = map.get(rootValue);
+
+        int leftTreeNodeSize = inorderIndex - inorderLeft;
+
+        TreeNode node = new TreeNode(rootValue);
+        node.left = buildTreeInner(map, preorder, preorderLeft + 1, preorderLeft + leftTreeNodeSize, inorderLeft, inorderIndex - 1);
+        node.right = buildTreeInner(map, preorder, preorderLeft + leftTreeNodeSize + 1, preorderRight, inorderIndex + 1, inorderRight);
+
+        return node;
+    }
+
+    public static void preorderPrint(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        print(root.value);
+        preorderPrint(root.left);
+        preorderPrint(root.right);
+    }
+
+    public static void inorderPrint(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        inorderPrint(root.left);
+        print(root.value);
+        inorderPrint(root.right);
+    }
+
+    public static void postorderPrint(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        postorderPrint(root.left);
+        postorderPrint(root.right);
+        print(root.value);
+    }
+
+    private static void print(int i) {
+        System.out.print(i);
+        System.out.print(',');
+    }
+
+    //====0522=====
+
+
 
 
     public static void main(String[] args) {
-        //0522
-        String input = "addeeefssfsfweesdefed";
         Solution202005 solution = new Solution202005();
+        //0522
+        /*
+        String input = "addeeefssfsfweesdefed";
         String output = solution.longestPalindrome(input);
         System.out.println(output);
-
-
+        */
+        /*
+        int [] preorder = new int[] {1, 2, 4, 5, 7, 8, 3, 6};
+        int[] inorder = new int[] {4, 2, 7, 5, 8, 1, 3, 6};
+        TreeNode treeNode = solution.buildTree(preorder, inorder);
+        preorderPrint(treeNode);
+        System.out.println();
+        inorderPrint(treeNode);
+        System.out.println();
+        postorderPrint(treeNode);
+         */
     }
 }
